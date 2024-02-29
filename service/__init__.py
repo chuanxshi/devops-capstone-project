fc71d77d0afc7417a2fd5406ac075e8ef6e89518
Chuan Shi <shichuanr@outlook.com>
2024-02-28 20:55:46 -0500

    added security headers and CORS policies

--- service/__init__.py
+++ service/__init__.py
@@ -8,7 +8,8 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
-
+from flask_talisman import Talisman
+from flask_cors import CORS
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
@@ -23,6 +24,9 @@ from service.common import error_handlers, cli_commands  # noqa: F401 E402
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
+# Create an instance of Talisman and pass the Flask app to it
+talisman = Talisman(app)
+CORS(app)
 app.logger.info(70 * "*")
 app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
